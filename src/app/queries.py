f22a1073cdf5098d5bce22162f62da373d6436b8
Kelli Scheuble <kellischeuble@gmail.com>
2020-05-27 13:08:03 -0700

    Splits up formatting function to allow reusability between queries

--- src/app/queries.py
+++ src/app/queries.py
@@ -6,6 +6,24 @@ es_name = os.environ["ES_NAME"]
 es_pass = os.environ["ES_PASS"]
 es_endpoint = os.environ["ES_ENDPOINT"]
 
+def reformat(response_query):
+    """
+    reformats elasticsearch query to remove unnecessary information
+    """
+
+    data = list()
+    for hit in response_query['hits']['hits']:
+        data.append({
+            'id': hit['_id'], 
+            'source_url' : hit['_source']['post_url'], 
+            'title': hit['_source']['title'], 
+            'description': hit['_source']['description'], 
+            'date_published': hit['_source']['publication_date'], 
+            'location_raw': hit['_source']['location_raw'], 
+            'geo_locat': hit['_source']['location_point']})
+    
+    return {'jobs':data}
+
 def get_all_jobs():
     """Simple Elasticsearch query that will return all jobs"""
     
@@ -23,18 +41,6 @@ def get_all_jobs():
     response = requests.get(uri, headers=headers, data=query)
     response = response.json()
     
-    # format response
-    data = list()
-    for hit in response['hits']['hits']:
-        data.append({
-            'id': hit['_id'], 
-            'source_url' : hit['_source']['post_url'], 
-            'title': hit['_source']['title'], 
-            'description': hit['_source']['description'], 
-            'date_published': hit['_source']['publication_date'], 
-            'location_raw': hit['_source']['location_raw'], 
-            'geo_locat': hit['_source']['location_point']})
-    
-    reformatted = {'jobs':data}
+    reformatted = reformat(response)
     
     return reformatted
