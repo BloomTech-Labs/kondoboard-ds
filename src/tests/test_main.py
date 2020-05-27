f7116b03d543e90c1f4c4dca6a0b6ce7e10c2d3b
Kelli Scheuble <kellischeuble@gmail.com>
2020-05-27 13:40:00 -0700

    Adds test to make sure status code = 200

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -4,8 +4,19 @@ from app.main import app
 
 client = TestClient(app)
 
+# def test_ping(test_app):
+#     response = test_app.get("/ping")
+#     assert response.status_code == 200
+#     assert response.json() == {"ping": "pong!"}
 
-def test_ping(test_app):
-    response = test_app.get("/ping")
-    assert response.status_code == 200
-    assert response.json() == {"ping": "pong!"}
+def test_all(test_app):
+    response = test_app.get("/all")
+    assert response.status_code ==200
+
+def test_track(test_app):
+    response = test_app.get("/track/{track}")
+    assert response.status_code ==200
+
+def test_custom_search(test_app):
+    response = test_app.get("/search/{search}/location/{location}")
+    assert response.status_code ==200
